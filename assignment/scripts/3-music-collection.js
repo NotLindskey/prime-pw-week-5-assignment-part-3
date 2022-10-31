b3381564dd482653642497144dbc1003be866019
Linssi Colin Ruiz <linssi101@gmail.com>
2022-10-31 18:07:53 -0500

    function addToCollection. Test the addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+	return collection.push({ title, artist, yearPublished });
+}
+
+addToCollection("Turn the Light On", "Imminence", 2019);
+addToCollection("Use Me (Deluxe)", "PVRIS", 2020);
+addToCollection("Disgusting", "Beartooth", 2014);
+addToCollection("Sempiternal", "Deluxe Edition", 2013);
+addToCollection("All We Know of Heaven, All we Need of Hell", "PRIVS", 2017);
+addToCollection("Palisades", "Palisades", 2017);
+
+console.log(collection[0]);
+console.log(collection[1]);
+console.log(collection[2]);
+console.log(collection[3]);
+console.log(collection[4]);
+console.log(collection[5]);
+
+console.log(collection);
